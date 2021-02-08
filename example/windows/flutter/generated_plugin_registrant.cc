27fe4e3cba0265a5c638c164e1a0f9f045b3e03d
wangkun <wangkun@woodemi.com>
2021-02-08 13:38:05 +0800

    Initial commit for Android & Windows
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <quick_usb/quick_usb_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  QuickUsbPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("QuickUsbPlugin"));
+}
