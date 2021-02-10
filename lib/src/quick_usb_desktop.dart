20861194f5bdeb35c9a73ad7a16e2be04934f4bd
wangkun <wangkun@woodemi.com>
2021-02-10 21:35:52 +0800

    Impl init/exit on macOS
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- lib/src/quick_usb_desktop.dart
+++ lib/src/quick_usb_desktop.dart
@@ -1,21 +1,34 @@
+import 'dart:ffi';
+
+import 'package:libusb/libusb64.dart';
+
 import 'quick_usb_platform_interface.dart';
 
-class QuickUsbWindows extends _QuickUsbDesktop {}
+class QuickUsbWindows extends _QuickUsbDesktop {
+  QuickUsbWindows() : super(null);
+}
 
-class QuickUsbMacos extends _QuickUsbDesktop {}
+class QuickUsbMacos extends _QuickUsbDesktop {
+  QuickUsbMacos() : super(DynamicLibrary.open('libusb-1.0.23.dylib'));
+}
 
-class QuickUsbLinux extends _QuickUsbDesktop {}
+class QuickUsbLinux extends _QuickUsbDesktop {
+  QuickUsbLinux() : super(null);
+}
 
 class _QuickUsbDesktop extends QuickUsbPlatform {
+  final Libusb _libusb;
+
+  _QuickUsbDesktop(DynamicLibrary dynamicLibrary)
+      : _libusb = Libusb(dynamicLibrary);
+
   @override
-  Future<bool> init() {
-    // TODO: implement init
-    throw UnimplementedError();
+  Future<bool> init() async {
+    return _libusb.libusb_init(nullptr) == libusb_error.LIBUSB_SUCCESS;
   }
 
   @override
-  Future<void> exit() {
-    // TODO: implement exit
-    throw UnimplementedError();
+  Future<void> exit() async {
+    return _libusb.libusb_exit(nullptr);
   }
 }
